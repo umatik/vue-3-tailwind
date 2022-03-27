7f3b5883c127d23c8a0d5bb9311ccc7028202902
Maciek Nierychlewski <maciej.nierychlewski@bravelab.io>
2022-03-27 15:16:33 +0200

    first changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,8 @@
 <template>
-  <h1 class="font-bold bg">Alamakota</h1>
+  <div class="columns-2">
+    <p>Well, let me tell you something, ...</p>
+    <p class="break-inside-avoid-column">Sure, go ahead, laugh...</p>
+    <p>Maybe we can live without...</p>
+    <p>Look. If you think this is..1</p>
+  </div>
 </template>
-AACg7QgpsCd8n3c
